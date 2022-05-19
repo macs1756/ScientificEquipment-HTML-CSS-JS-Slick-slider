24860485df46b8bbc2991ed1891eb5614206c99e
macs1756 <kindrativmaksum312@gmail.com>
2022-05-19 20:24:19 +0200

    trty

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const equipment = document.querySelector("#equipment");
 const equipmentContent = document.querySelector("#equipment__content");
 const language = document.querySelector("#language");
 const languageBtn = document.querySelector("#language__btn");
+const burgerBtn = document.querySelector("#burgerBtn");
 
 equipment.addEventListener("click", function(){
 	equipmentContent.classList.toggle("opa__list");
@@ -10,4 +11,10 @@ equipment.addEventListener("click", function(){
 
 languageBtn.addEventListener("click", function(){
 	language.classList.toggle("opa__list");
+});
+
+
+
+burgerBtn.addEventListener("click", function(){
+	burgerBtn.classList.toggle("btnClose");
 });
